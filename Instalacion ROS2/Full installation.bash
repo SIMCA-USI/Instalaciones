95f07dea6c4366770dcd07ac5da8832e32c797c3
Alfredo Valle Barrio <alfredo.valle@upm.es>
2020-05-10 18:05:03 +0200

    Fix some errors
    git clone is asyncronous 
    More flexible script only select ros2 version and repositories

--- Instalacion ROS2/Full installation.bash
+++ Instalacion ROS2/Full installation.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Se recogen las contraseñas necesarias
 read -s -p "Enter sudo password : " sudopass
+echo ""
 read -p "Enter your Github user : " user
+echo ""
 read -s -p "Enter your Github pass : " pass
+echo ""
+export $CHOOSE_ROS_DISTRO=eloquent
 # Se accede con sudo y se actualizan los repositorios
 echo $sudopass | sudo -S apt update
 # Se instalan librerías basicas que necesitaremos  mas adelante
@@ -10,30 +14,40 @@ sudo apt install curl gnupg2 lsb-release python3-argcomplete git python3-pip pyt
 # Se inicializa y actualiza rosdep para dependencias de paquetes de ros
 sudo rosdep init
 rosdep update
-# Se descarga eloquen y se instala junto con rqt y el compilador colcon
+# Se descarga eloquent y se instala junto con rqt y el compilador colcon
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update
-sudo apt install ros-eloquent-desktop ros-eloquent-rqt-* python3-colcon-common-extensions -y
+sudo apt install ros-$CHOOSE_ROS_DISTRO-desktop ros-$CHOOSE_ROS_DISTRO-rqt-* python3-colcon-common-extensions -y
+# Una vez instalado eloquent se instala rosbag
+sudo apt install ros-$ROS_DISTRO-ros2bag* ros-$ROS_DISTRO-rosbag2*
 # Se añade eloquent al bash y se identifica el ordenador con un id
-source /opt/ros/eloquent/setup.bash
-echo "source /opt/ros/eloquent/setup.bash" >> ~/.bashrc
+source /opt/ros/$ROS_DISTRO/setup.bash
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 echo "export ROS_DOMAIN_ID=2" >> ~/.bashrc
 # Se crea el workspace
 mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
 # Se instalan algunos programas utiles
 sudo snap install gitkraken
 sudo snap install pycharm-community --classic
+#dhcp 
+sudo snap install sublime-text --classic
 # Se instala la libreria bezier porque da problemas en la instalación como dependencia
-BEZIER_NO_EXTENSION=true
+export BEZIER_NO_EXTENSION=true
 pip3 install bezier
 # Se descargan los repositorios necesarios y se instalan las dependencias
-git clone https://$user:$pass@github.com/SIMCA-USI/ros2_waypoints.git
-cd ros2_waypoints && pip3 install . && cd ..
-git clone https://$user:$pass@github.com/SIMCA-USI/ros2_gps.git
-cd ros2_gps && pip3 install . && cd ..
-git clone https://$user:$pass@github.com/SIMCA-USI/ros_can.git
-cd ros_can && pip3 install . && cd ..
+git clone https://$user:$pass@github.com/SIMCA-USI/ros2_waypoints.git && echo "Cloned"
+git clone https://$user:$pass@github.com/SIMCA-USI/ros2_gps.git && echo "Cloned"
+git clone https://$user:$pass@github.com/SIMCA-USI/ros_can.git && echo "Cloned"
+# Se recorren todos los directorios de la carpeta source y se instalan las dependencias de python
+for D in *; do
+	if [ -d "${D}" ]; then
+		cd "${D}"
+		pip3 install .
+		cd ..
+	fi
+done
 # Se instalan las dependencias de paquetes de ros
 cd ~/ros2_ws
 sudo rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
